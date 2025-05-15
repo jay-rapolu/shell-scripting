8cf5753c9a17d169729ca6fa091ace92ef0774d6
jay-rapolu <jay.rapolu@outlook.com>
2025-05-15 19:02:22 +0530

    creating arrays

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#defining Array
+Movies=("Thandel" "Hi Nanna" "HIT 3" "Pushpa 2" "Court") 
+
+#Accessing Array (Index Starts from 0)
+
+#Accessing the nth Element Movies[n]
+
+echo "Last movie in the list is $Movies[-1]"

eac385817e93c95251f18d550676a2f329b8533f
jay-rapolu <jay.rapolu@outlook.com>
2025-05-15 18:18:16 +0530

    passing variables from arguments

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2! Long time no see. How have you been?"
+echo "$PERSON2:: Hi $PERSON1! I’ve been good, just caught up with work. How about you?"
+echo "$PERSON1:: Same here. Work’s been hectic lately. By the way, did you watch the cricket match last night?"
+echo "$PERSON2:: Oh yes! What a thriller! I couldn’t believe that last-over finish. My heart was racing!"
+echo "$PERSON1:: I know, right? That six on the second-last ball was epic!"
+echo "$PERSON2:: Totally! We should catch the next match together. It’ll be fun."
+echo "$PERSON1:: Sounds like a plan. Let’s do it this weekend!"

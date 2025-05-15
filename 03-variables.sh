bebeafd493ceed4121edfafe11c776f32b2d22e9
jay-rapolu <jay.rapolu@outlook.com>
2025-05-15 18:16:10 +0530

    using variable for conversation

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,10 +3,10 @@
 PERSON1=Jay
 PERSON2=Anvesh
 
-echo "Ramesh:: Hey Suresh! Long time no see. How have you been?"
-echo "Suresh:: Hi Ramesh! I’ve been good, just caught up with work. How about you?"
-echo "Ramesh:: Same here. Work’s been hectic lately. By the way, did you watch the cricket match last night?"
-echo "Suresh:: Oh yes! What a thriller! I couldn’t believe that last-over finish. My heart was racing!"
-echo "Ramesh:: I know, right? That six on the second-last ball was epic!"
-echo "Suresh:: Totally! We should catch the next match together. It’ll be fun."
-echo "Ramesh:: Sounds like a plan. Let’s do it this weekend!"
+echo "$PERSON1:: Hey $PERSON2! Long time no see. How have you been?"
+echo "$PERSON2:: Hi $PERSON1! I’ve been good, just caught up with work. How about you?"
+echo "$PERSON1:: Same here. Work’s been hectic lately. By the way, did you watch the cricket match last night?"
+echo "$PERSON2:: Oh yes! What a thriller! I couldn’t believe that last-over finish. My heart was racing!"
+echo "$PERSON1:: I know, right? That six on the second-last ball was epic!"
+echo "$PERSON2:: Totally! We should catch the next match together. It’ll be fun."
+echo "$PERSON1:: Sounds like a plan. Let’s do it this weekend!"

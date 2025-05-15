e3c1da79b4defb127c126e007dd6a6009fe5b0d3
jay-rapolu <jay.rapolu@outlook.com>
2025-05-15 18:41:46 +0530

    adding two numbers

--- 05-variable.sh
+++ 05-variable.sh
@@ -5,3 +5,5 @@ read user
 echo "Please enter the password: "
 read -s pass
 echo "Thanks for details, welcome $user"
+
+#read -p "Hello User, Please enter your email id or phone number: " user -> you can use this if you want to display before read instead of echo.

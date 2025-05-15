f82fa02c9b286560f3b0af393f7a469fc9df3785
jay-rapolu <jay.rapolu@outlook.com>
2025-05-15 18:10:31 +0530

    converstaion between two persons

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Ramesh:: Hey Suresh! Long time no see. How have you been?"
+echo "Suresh:: Hi Ramesh! I’ve been good, just caught up with work. How about you?"
+echo "Ramesh:: Same here. Work’s been hectic lately. By the way, did you watch the cricket match last night?"
+echo "Suresh:: Oh yes! What a thriller! I couldn’t believe that last-over finish. My heart was racing!"
+echo "Ramesh:: I know, right? That six on the second-last ball was epic!"
+echo "Suresh:: Totally! We should catch the next match together. It’ll be fun."
+echo "Ramesh:: Sounds like a plan. Let’s do it this weekend!"

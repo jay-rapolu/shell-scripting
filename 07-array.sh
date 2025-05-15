21c5d3f814002942f8c0b080d01523b30ba76061
jay-rapolu <jay.rapolu@outlook.com>
2025-05-15 19:03:32 +0530

    accessing arrays

--- 07-array.sh
+++ 07-array.sh
@@ -5,6 +5,6 @@ Movies=("Thandel" "Hi Nanna" "HIT 3" "Pushpa 2" "Court")
 
 #Accessing Array (Index Starts from 0)
 
-#Accessing the nth Element Movies[n]
+#Accessing the nth Element Movies[n] by default it will pick the first item
 
-echo "Last movie in the list is $Movies[-1]"
+echo "Last movie in the list is ${Movies[-1]}" 

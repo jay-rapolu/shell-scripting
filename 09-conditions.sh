ce6394cbeeb0f0515afc91496cf320a59609a29d
jay-rapolu <jay.rapolu@outlook.com>
2025-05-16 21:41:52 +0530

    conditional statements

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-NUMBER=$1
-if [ $NUMBER == "" ]
-then
-    echo "Please pass a number as first argument along with the script to execute it"
-    exit 1
-fi
-
-echo "This Script is used to check if the entered number is positive or negative"
-echo ""
-
 # There are six conditional operators in shell script they are: 
 # -gt -> greater than
 # -lt -> less than
@@ -18,9 +8,16 @@ echo ""
 # -le -> less than or Equals
 # -ne -> not equals 
 
-if [ $1 -ge 0 ]
+echo "This Script is used to check if the entered number is positive or negative"
+
+if [[ -n "$1" && "$1" -eq "$1" 2>/dev/null ]]
 then
-    echo "entered number $1 is a positive number"
+    if [ $1 -ge 0 ]
+    then
+        echo "entered number $1 is a positive number"
+    else
+        echo "entered number $1 is a negative number"
+    fi
 else
-    echo "entered number $1 is a negative number"
+    echo "Please enter a numeric value"
 fi

4e81fb0b6d4c75eda0235cbe83c6223821b8969e
jay-rapolu <jay.rapolu@outlook.com>
2025-05-17 20:27:15 +0530

    functions practice

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
-PACKAGE=$1
 
 if [ $# -eq 0 ]
 then
-    echo "Please enter a package name as first argument"
+    echo "Please enter atleast one package name."
     exit 1
 fi
 
@@ -17,7 +16,20 @@ else
     exit 1
 fi
 
-dnf list installed $PACKAGE > /dev/null
+VALIDATE() {
+    if [ $1 -eq 0 ]
+    then
+        echo "Package is available"
+        echo "Installing the Package"
+        dnf install $2 -y
+        echo "Package Installed Successfully"
+    else
+        echo "entered package name is incorrect or package name is not available."
+        exit 1
+    fi
+}
+
+dnf list installed $1 > /dev/null
 
 if [ $? -eq 0 ]
 then
@@ -25,14 +37,5 @@ then
 else
     echo "checking if the package is availble or not"
     dnf list available $PACKAGE > /dev/null
-    if [ $? -eq 0 ]
-    then
-        echo "Package is available"
-        echo "Installing the Package"
-        dnf install $PACKAGE -y
-        echo "Package Installed Successfully"
-    else
-        echo "entered package name is incorrect or package name is not available."
-        exit 1
-    fi
+    VALIDATE() $? $1
 fi

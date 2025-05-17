918574b1e43234acee232ad7fd6329235933a475
jay-rapolu <jay.rapolu@outlook.com>
2025-05-17 07:48:19 +0530

    shell script practice

---
+++ 10-conditions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+PACKAGE=$1
+
+if [ $USERID -eq 0 ]
+then
+    echo "Script is Running with root permissions.. proceeding for package installation"
+else
+    echo "Please run the script with root user permissions.. exiting the script"
+    exit 1
+fi
+
+dnf list installed | $PACKAGE
+
+if [ $? == 0 ]
+then
+    echo "The Package is already installed in the machine"
+    exit 1
+else
+    echo "checking if the package is availble or not"
+    dnf list installed | grep -i $PACKAGE
+    if[ $? == 0 ]
+    then
+        echo "Installing the Package"
+        dnf install $PACKAGE -y
+    else
+        echo "entered package name is incorrect or package name is not available."
+        exit 1
+fi

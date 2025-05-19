b6e3015b8e77cc49325478f4e7d5a9b06c082cbe
jay-rapolu <jay.rapolu@outlook.com>
2025-05-19 17:49:48 +0530

    tee practice

--- 16-package-installation-loops.sh
+++ 16-package-installation-loops.sh
@@ -5,35 +5,42 @@ LOG_DIR=/var/log/shell-script
 FILE_NAME=$(echo $0 | cut -d '.' -f1)
 LOG_PATH=/var/log/shell-script/$FILE_NAME.log
 PACKAGES=("mysql" "nginx" "httpd" "maven")
+
+#Colors
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+D='\e[0m'
+
 mkdir -p $LOG_DIR
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error:: Please run the script as root user." 
+    echo -e "${R}Error::${D} Please run the script as root user." | tee -a $LOG_PATH
     exit 1
 else
-    echo "Script is running as root user.. Proceeding to Installation."
+    echo -e "${Y}Starting Script::${D} Script is running as root user.. Proceeding to Installation." | tee -a $LOG_PATH
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Error:: $2 Package Installation was unsuccessful" | tee -a $LOG_PATH
+        echo -e "${R}Error::${D} $2 Package Installation was unsuccessful" | tee -a $LOG_PATH
         exit 1
     else
-        echo "Success:: $2 Package Installation was successfully done."
+        echo -e "${G}Success::${D} $2 Package Installation was successfully done." | tee -a $LOG_PATH
     fi
 }
 
 for package in ${PACKAGES[@]}
 do
-    dnf list installed $package
+    dnf list installed $package &>> $LOG_PATH
     if [ $? -ne 0 ]
     then
-        echo "Package is not Installed.. Installing the Package." | tee -a $LOG_PATH
+        echo -e "Package is not Installed.. ${Y}Installing the Package.${D}" | tee -a $LOG_PATH
         dnf install $package -y &>> $LOG_PATH
-        VALIDATE $? $package
+        VALIDATE $? $package 
     else
-        echo "Package is available.. no need to install."
+        echo -e "${Y}Available::${D} Package is available.. no need to install." | tee -a $LOG_PATH
     fi
 done

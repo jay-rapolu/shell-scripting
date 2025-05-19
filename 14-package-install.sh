838882b470bf4276fa569fb253db914856f546af
jay-rapolu <jay.rapolu@outlook.com>
2025-05-19 16:49:01 +0530

    colors practice

---
+++ 14-package-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID = $(id -u)
+
+if [ $USERID -ne 0]
+then
+    echo "Error:: Please run the script as root user."
+    exit 1
+else
+    echo "Script is running as root user.. Proceeding to Installation."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "Package is not Installed.. Installing the Package."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Package Installation was unsuccessful"
+        exit 1
+    else
+        echo "Package Installation was successfully done."
+    fi
+else
+    echo "Package is available.. no need to install."
+fi

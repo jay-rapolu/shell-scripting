1bf64cc56f03a5d98370aaabc666a0e5ece16f33
jay-rapolu <jay.rapolu@outlook.com>
2025-05-19 20:12:50 +0530

    instance automation using shell

--- aws/instance-automation.sh
+++ aws/instance-automation.sh
@@ -2,5 +2,11 @@
 
 SG_ID=sg-0397920120499858a
 IMG_ID=ami-09c813fb71547fc4f
+INSTANCES=("mysql" "mongodb" "redis" "rabbitmq" "catalogue" "user" "cart" "payment" "shipping" "dispatch" "frontend")
 
-aws ec2 run-instances --image-id $IMG_ID --instance-type t2.micro --security-group-id $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyInstance}]"
+for instance in ${INSTANCES[@]}
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $IMG_ID --instance-type t2.micro --security-group-id $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query Instances[0].InstanceId --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)  
+    echo $instance IP Address : $IP
+done

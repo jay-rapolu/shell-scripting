9084708a6506aa31e1ce17475ea53356ba618771
jay-rapolu <jay.rapolu@outlook.com>
2025-05-19 18:24:09 +0530

    aws automation using shell practice fix

--- aws/instance-automation.sh
+++ aws/instance-automation.sh
@@ -3,4 +3,4 @@
 SG_ID=sg-0397920120499858a
 IMG_ID=ami-09c813fb71547fc4f
 
-aws ec2 run-instances --image-id $IMG_ID --instance-type t2.micro --security-group-ids $SG_ID --tags Key=Name,Value=MyInstance
+aws ec2 run-instances --image-id $IMG_ID --instance-type t2.micro --security-group-id $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyInstance}]"

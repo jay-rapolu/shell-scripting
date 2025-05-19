9a387cf157ef54d6c75ee2455a1dfa56b3bf8338
jay-rapolu <jay.rapolu@outlook.com>
2025-05-19 16:30:17 +0530

    colors practice

--- 13-colors.sh
+++ 13-colors.sh
@@ -3,6 +3,6 @@
 R='\e[31m'
 D='\e[0m'
 
-echo -e "$R\This line should be printed in red color$D"
+echo -e "${R}This line should be printed in red color$D"
 
 echo "This line should be printed in normal color"
